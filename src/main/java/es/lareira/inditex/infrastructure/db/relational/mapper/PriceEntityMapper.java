992c7751f200a2cb947780bf7e436154a7ad3ade
Carlos Bouzón García <carlos.bouzon.garcia@gmail.com>
2023-10-12 19:51:27 +0200

    #24 add integration tests

--- src/main/java/es/lareira/inditex/infrastructure/db/relational/mapper/PriceEntityMapper.java
+++ src/main/java/es/lareira/inditex/infrastructure/db/relational/mapper/PriceEntityMapper.java
@@ -2,6 +2,7 @@ package es.lareira.inditex.infrastructure.db.relational.mapper;
 
 import es.lareira.inditex.application.domain.model.price.Price;
 import es.lareira.inditex.infrastructure.db.relational.entity.PriceEntity;
+import java.math.BigDecimal;
 import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,6 +10,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface PriceEntityMapper {
 
+  @Mapping(target = "finalPrice", source = "price")
   @Mapping(target = "brandId", source = "brandEntity.id")
   @Mapping(target = "rateId", source = "priceList")
   @Mapping(target = "applicationDateRange.from", source = "startDate")
